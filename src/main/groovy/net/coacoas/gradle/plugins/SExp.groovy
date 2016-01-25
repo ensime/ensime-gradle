c8c741a1b434f9db4c82fdcf2e7a352f424993d4
Bill Carlson <bcarlson@vorrohealth.com>
2016-01-25 13:48:49 -0500

    Taking advantage of Groovy's runtime dispatch

--- src/main/groovy/net/coacoas/gradle/plugins/SExp.groovy
+++ src/main/groovy/net/coacoas/gradle/plugins/SExp.groovy
@@ -4,19 +4,27 @@ package net.coacoas.gradle.plugins
  * Transform a configuration object into stringified Lisp expressions
  */
 class SExp {
+  static String format(Map map) { 
+      map.isEmpty() ? 'nil' : '(' + map.entrySet().collect { ":${it.key} " + format(it.value)}.join('\n') + ')'
+  }
+
+  static String format(List list) { 
+      list.isEmpty() ? 'nil' : '(' + list.collect { format(it) }.join(' ') + ')'
+  }
+
+  static String format(String string) { 
+    "\"${string.replaceAll("\\\\", "\\\\\\\\")}\""
+  }
+
+  static String format(Boolean bool) {
+    bool ? 't' : 'nil'
+  }
+
+  static String format(Integer integer) {
+    "${integer}"
+  }
+
   static String format(Object object) {
-    if (object instanceof Map) {
-      return object.isEmpty() ? 'nil' : '(' + object.entrySet().collect { ":${it.key} " + format(it.value)}.join('\n') + ')'
-    } else if (object instanceof List) {
-      return object.isEmpty() ? 'nil' : '(' + object.collect { format(it) }.join(' ') + ')'
-    } else if (object instanceof String) {
-      return '"' + object.toString().replaceAll("\\\\", "\\\\\\\\") + '"'
-    } else if (object instanceof Boolean) {
-      return object ? "t" : "nil"
-    } else if (object instanceof Integer) {
-      return "${object}"
-    } else {
-      return "\"${object}\""
-    }
+     "\"${object}\""
   }
 }
